8f37aadf09786da27a63f10168b78ab7d336fdf4
olelwow <ristenstrand66@gmail.com>
2023-12-04 13:06:39 +0100

    wack

--- Day2/index.js
+++ Day2/index.js
@@ -1,31 +1,50 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./example.txt", "utf-8").trim().split("\r\n");
+const input = fs.readFileSync("./input.txt", "utf-8").trim().split("\r\n");
 
 let ID = 0;
+let correctIDS = 0;
 let colors = { red: 0, blue: 0, green: 0 };
-let game = {};
+let game = [[],[]];
+
 
 for (const element of input) {
-  game = element.split(": ")[1].split("; ");
+  ID++;
+  game[ID] = element.split(": ")[1].split("; ");
+  
 }
 
 const toNumber = (value) => {
   return Number(value);
 };
 
-for (const element of game) {
-  let amount = element.match(/\d/g).map(toNumber);
-  let color = element.match(/\b([a-zA-Z]+)\b/g);
-  //        /b för att få ut hela ord, ([a-zA-Z]+) fångar orden i grupp.
+for (let i = 0; i < game.length; i++) {
+  colors['red'] = 0;
+  colors['green'] = 0;
+  colors['blue'] = 0;
+  for (const element of game[i]) {
+    let amount = element.match(/\d/g).map(toNumber);
+    
+   
+    
+    let color = element.match(/\b([a-zA-Z]+)\b/g);
+    //        /b för att få ut hela ord, ([a-zA-Z]+) fångar orden i grupp.
+  
 
-  for (let i = 0; i < color.length; i++) {
-    colors[color[i].trim()] += amount[i];
+    for (let j = 0; j < color.length; j++) {
+      colors[color[j].trim()] += amount[j];
+      
+    }
+    
   }
-
-  if (colors["red"] > 12 || colors["green"] > 13 || colors["blue"] > 14) {
-    ID++;
-  }
+  if (colors["red"] >= 13 || colors["green"] >= 14 || colors["blue"] >= 15) {
+    
+    }
+    else {
+      correctIDS += i;
+    }
 }
 
-console.log(game);
+
+
+console.log(correctIDS);

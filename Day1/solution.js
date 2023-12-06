6b61d10338c00177d29e9135acc7427889a146cb
Olle Ristenstrand <olelwow@hotmail.com>
2023-12-06 16:15:04 +0100

    Dag 2 avklarad.

--- Day1/solution.js
+++ Day1/solution.js
@@ -1,39 +1,59 @@
 import fs from "fs";
 
-const inputLines = fs.readFileSync("./input.txt", "utf-8").trim().split("\r\n");
+const inputLines = fs
+  .readFileSync("./example.txt", "utf-8")
+  .trim()
+  .split("\r\n");
 
 console.log(inputLines);
 
 let numbers = [[], []];
+let wordsToNumbers = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+let numbersInWords = [[], []];
 let tempResult = [];
 let result = 0;
 
 for (let i = 0; i < inputLines.length; i++) {
   for (const element of inputLines[i]) {
-    numbers[i] = inputLines[i].match(/\d/g);
+    const line = element.search(
+      /\b:one|two|three|four|five|six|seven|eight|nine|\b/g
+    );
+    console.log(line);
+    // numbers[i] = inputLines[i].match(
+    //   /\b:one|two|three|four|five|six|seven|eight|nine|\b/g
+    // );
+
+    // numbers[i + 1] = inputLines[i].match(/\d/g);
     // \d/g tar ut en siffra i taget.
   }
   // Går igenom alla siffror på rad [i] i inputLines och lägger dem på rad [i] i numbers.
 }
-
-for (let i = 0; i < numbers.length; i++) {
-  for (const element of numbers[i]) {
-    numbers[i].length < 2
-      ? (tempResult[i] = numbers[i][0] + numbers[i][0])
-      : (tempResult[i] = numbers[i][0] + numbers[i][numbers[i].length - 1]);
-  }
-}
-
-const toNumber = (value) => {
-  return Number(value);
-};
-tempResult = tempResult.map(toNumber);
-// Gör om strängarna till siffror för att kunna utföra addition senare.
-
-tempResult.forEach((element) => {
-  result += element;
-});
-
 console.log(numbers);
-console.log(tempResult);
-console.log(result);
+
+// for (let i = 0; i < numbers.length; i++) {
+//   for (const element of numbers) {
+//     numbers.length < 2
+//       ? (tempResult[i] = numbers[0] + numbers[0])
+//       : (tempResult[i] = numbers[0] + numbers[numbers.length - 1]);
+//   }
+// }
+
+// const toNumber = (value) => {
+//   return Number(value);
+// };
+// tempResult = tempResult.map(toNumber);
+// // Gör om strängarna till siffror för att kunna utföra addition senare.
+
+// tempResult.forEach((element) => {
+//   result += element;
+// });
--- Day2/solution.js
+++ Day2/solution.js
@@ -2,49 +2,84 @@ import fs from "fs";
 
 const input = fs.readFileSync("./input.txt", "utf-8").trim().split("\r\n");
 
-let ID = 0;
-let correctIDS = 0;
-let colors = { red: 0, blue: 0, green: 0 };
-let game = [[],[]];
+const partOne = (input) => {
+  let ID = 1;
+  let correctIDS = 0;
+  let colors = { red: 0, blue: 0, green: 0 };
+  let game = [];
 
-
-for (const element of input) {
-  ID++;
-  game[ID] = element.split(": ")[1].split("; ");
-  
-}
-
-const toNumber = (value) => {
-  return Number(value);
-};
-
-for (let i = 0; i < game.length; i++) {
-  colors['red'] = 0;
-  colors['green'] = 0;
-  colors['blue'] = 0;
-  for (const element of game[i]) {
-    let amount = element.match(/\d/g).map(toNumber);
-    
-   
-    
-    let color = element.match(/\b([a-zA-Z]+)\b/g);
-    //        /b för att få ut hela ord, ([a-zA-Z]+) fångar orden i grupp.
-  
-
-    for (let j = 0; j < color.length; j++) {
-      colors[color[j].trim()] += amount[j];
-      
-    }
-    
+  for (const element of input) {
+    game[ID] = element.split(": ")[1].split("; ");
+    ID++;
   }
-  if (colors["red"] >= 13 || colors["green"] >= 14 || colors["blue"] >= 15) {
-    
+  const toNumber = (value) => {
+    return Number(value);
+  };
+
+  for (let i = 1; i < game.length; i++) {
+    for (const element of game[i]) {
+      colors["red"] = 0;
+      colors["green"] = 0;
+      colors["blue"] = 0;
+
+      let amount = element.match(/\d+/g).map(toNumber);
+
+      let color = element.match(/\b([a-zA-Z]+)\b/g);
+      //        /b för att få ut hela ord, ([a-zA-Z]+) fångar orden i grupp.
+
+      for (let j = 0; j < color.length; j++) {
+        colors[color[j]] = amount[j];
+      }
+      if (colors["red"] > 12 || colors["green"] > 13 || colors["blue"] > 14) {
+        break;
+      }
     }
-    else {
+    if (colors["red"] <= 12 && colors["green"] <= 13 && colors["blue"] <= 14) {
       correctIDS += i;
     }
-}
+  }
+  console.log(correctIDS);
+};
 
+const partTwo = (input) => {
+  let ID = 1;
+  let correctIDS = 0;
+  let colors = { red: 0, blue: 0, green: 0 };
+  let game = [];
+  let result = 0;
 
+  for (const element of input) {
+    game[ID] = element.split(": ")[1].split("; ");
+    ID++;
+  }
 
-console.log(correctIDS);
+  const toNumber = (value) => {
+    return Number(value);
+  };
+
+  for (let i = 1; i < game.length; i++) {
+    colors["red"] = 0;
+    colors["green"] = 0;
+    colors["blue"] = 0;
+    let tempResult = 0;
+
+    for (const element of game[i]) {
+      let amount = element.match(/\d+/g).map(toNumber);
+
+      let color = element.match(/\b([a-zA-Z]+)\b/g);
+      //        /b för att få ut hela ord, ([a-zA-Z]+) fångar orden i grupp.
+
+      for (let j = 0; j < color.length; j++) {
+        if (colors[color[j]] <= amount[j]) {
+          colors[color[j]] = amount[j];
+        } else {
+          console.log(i);
+          console.log(colors);
+        }
+      }
+    }
+    tempResult = colors["blue"] * colors["red"] * colors["green"];
+    result += tempResult;
+  }
+  console.log(result);
+};
